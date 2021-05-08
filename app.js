ddbd2c841189dba531a10ffb7528e97a1308e53c
Himanshu Sharma <hs360257@gmail.com>
2021-05-08 17:05:36 +0530

    Added new files

---
+++ app.js
@@ -0,0 +1,20 @@
+const likeBtn = document.querySelector(".like_btn");
+let likeIcon = document.querySelector("#icon");
+let count = document.querySelector("#count");
+
+
+let clicked = false;
+
+
+likeBtn.addEventListener("click", () =>{
+    if(!clicked){
+        clicked = true;
+        likeIcon.innerHTML = '<i class="far fa-thumbs-up"></i>';
+        count.textContent++;
+    }
+    else{
+        clicked = false;
+        likeIcon.innerHTML = '<i class="far fa-thumbs-up"></i>';
+        count.textContent--;
+    }
+});
